0ccb2f3594721b7247ef39c537e9d916e48758ed
Dang Nguyen <nguyen05082004@gmail.com>
2026-01-18 13:41:44 +0700

    add topic api

---
+++ Services/Interfaces/ITopicService.cs
@@ -0,0 +1,13 @@
+﻿using ELearning_ToanHocHay_Control.Models.DTOs;
+
+namespace ELearning_ToanHocHay_Control.Services.Interfaces
+{
+    public interface ITopicService
+    {
+        Task<ApiResponse<IEnumerable<TopicDto>>> GetByChapterAsync(int chapterId);
+        Task<ApiResponse<TopicDto>> GetByIdAsync(int topicId);
+        Task<ApiResponse<TopicDto>> CreateAsync(CreateTopicDto dto);
+        Task<ApiResponse<TopicDto>> UpdateAsync(int topicId, UpdateTopicDto dto);
+        Task<ApiResponse<bool>> DeleteAsync(int topicId);
+    }
+}

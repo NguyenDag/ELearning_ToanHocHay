fce497390248e7308e10ee4ec6c6ed7e6725b846
Dang Nguyen <nguyen05082004@gmail.com>
2026-02-03 18:34:15 +0700

    update by migrate

---
+++ Migrations/20260203113303_UpdateQuestionAndPackage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ELearning_ToanHocHay_Control.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateQuestionAndPackage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EndTime",
+                table: "ExerciseAttempt",
+                newName: "SubmittedAt");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PlannedEndTime",
+                table: "ExerciseAttempt",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "ExerciseAttempt",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlannedEndTime",
+                table: "ExerciseAttempt");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ExerciseAttempt");
+
+            migrationBuilder.RenameColumn(
+                name: "SubmittedAt",
+                table: "ExerciseAttempt",
+                newName: "EndTime");
+        }
+    }
+}
